b1169be8affd0a335d89b9665416d7ee81c6af93
iarik <iaroslavlupu909@gmail.com>
2024-02-14 17:41:18 +0200

    first comit

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello")
+# class Student:
+#     print("hi")
+#     def __init__(self):
+#         self.height = 160
+#         print("i am alive")
+#         print(self)
+# first_student = Student()
+# second_student = Student()
+# third_student = Student()
+#
+# print(first_student.height)
+# Student.__init__(first_student)
+
+
+# class Student:
+#     def __init__(self, height1=160):
+#         self.height = height1
+#
+# nick = Student()
+# ket = Student(height1=170)
+# chiril = Student(height1=2833)
+# print(nick.height)
+# print(ket.height)
+# print(chiril.height)
